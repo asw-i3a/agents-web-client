ae8f2c87691a327701374d9f1cc2b84eae66050c
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-05-03 23:03:15 +0200

    hotfix: removed test environment from restcontrollertest
    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- src/test/java/io/github/asw/i3a/agentWebClient/rest/RESTControllerTest.java
+++ src/test/java/io/github/asw/i3a/agentWebClient/rest/RESTControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -43,7 +42,7 @@ import net.minidev.json.JSONObject;
 @SpringBootTest(classes = { Service.class })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Category(IntegrationTest.class)
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 public class RESTControllerTest {
 
 	@Autowired
